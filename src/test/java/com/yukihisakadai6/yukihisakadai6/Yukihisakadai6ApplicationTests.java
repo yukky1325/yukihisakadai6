b4b3056f2efd8e1928947f984be902dba8999d43
和田幸久 <yuki777_march@yahoo.co.jp>
2024-03-11 16:23:32 +0900

    エラー確認開始

---
+++ src/test/java/com/yukihisakadai6/yukihisakadai6/Yukihisakadai6ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yukihisakadai6.yukihisakadai6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Yukihisakadai6ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
